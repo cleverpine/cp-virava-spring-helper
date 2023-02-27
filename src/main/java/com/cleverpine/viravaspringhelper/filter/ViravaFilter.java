1998dd8ec16f141d221c61f05a5ec907642bbc41
Lyuboslav Ignatov <lyuboslav.ignatov@cleverpine.com>
2023-02-27 14:03:03 +0200

    token authentication added to the virava filter and version updated

--- src/main/java/com/cleverpine/viravaspringhelper/filter/ViravaFilter.java
+++ src/main/java/com/cleverpine/viravaspringhelper/filter/ViravaFilter.java
@@ -1,19 +1,15 @@
 package com.cleverpine.viravaspringhelper.filter;
 
-import com.auth0.jwt.JWT;
-import com.auth0.jwt.algorithms.Algorithm;
-import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.auth0.jwt.interfaces.DecodedJWT;
-import com.auth0.jwt.interfaces.JWTVerifier;
 import com.cleverpine.viravaspringhelper.config.AuthTokenConfig;
 import com.cleverpine.viravaspringhelper.config.RoleConfig;
+import com.cleverpine.viravaspringhelper.core.TokenAuthenticator;
 import com.cleverpine.viravaspringhelper.core.ViravaAuthenticationToken;
+import com.cleverpine.viravaspringhelper.error.exception.InvalidTokenAuthenticationException;
+import com.cleverpine.viravaspringhelper.error.exception.ViravaAuthenticationException;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import java.io.IOException;
-import java.util.Base64;
-import java.util.Map;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -22,9 +18,14 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import java.io.IOException;
+import java.util.Base64;
+import java.util.Map;
+
 public class ViravaFilter extends OncePerRequestFilter {
 
-    private static final String BEARER_PREFIX = "Bearer ";
+    public static final String BEARER_PREFIX = "Bearer ";
+    private static final String INVALID_ACCESS_TOKEN = "Authentication failed: Invalid access token";
 
     private final RoleConfig<?, ?> roleConfig;
 
@@ -32,52 +33,34 @@ public class ViravaFilter extends OncePerRequestFilter {
 
     private final AuthTokenConfig authTokenConfig;
 
-    public ViravaFilter(RoleConfig<?, ?> roleConfig, ObjectMapper objectMapper, AuthTokenConfig authTokenConfig) {
+    private final TokenAuthenticator<DecodedJWT> tokenAuthenticator;
+
+    public ViravaFilter(RoleConfig<?, ?> roleConfig, ObjectMapper objectMapper, AuthTokenConfig authTokenConfig, TokenAuthenticator<DecodedJWT> tokenAuthenticator) {
         this.roleConfig = roleConfig;
         this.objectMapper = objectMapper;
         this.authTokenConfig = authTokenConfig;
+        this.tokenAuthenticator = tokenAuthenticator;
     }
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         String authorizationHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
         if (authorizationHeader == null || !authorizationHeader.startsWith(BEARER_PREFIX)) {
-            setUnauthorizedAuthContext(null);
-            filterChain.doFilter(request, response);
-            return;
+            throw new InvalidTokenAuthenticationException(INVALID_ACCESS_TOKEN);
         }
         var tokenString = authorizationHeader.substring(BEARER_PREFIX.length());
         try {
-            DecodedJWT token = null;
-            if (authTokenConfig.getSecret() == null) {
-                token = JWT.decode(tokenString);
-            } else {
-                token = verify(tokenString, authTokenConfig.getSecret(), authTokenConfig.getIssuer());
-            }
-            var payload = new String(Base64.getDecoder().decode(token.getPayload()));
+            var jwt = tokenAuthenticator.process(tokenString);
+            var payload = new String(Base64.getDecoder().decode(jwt.getPayload()));
             Map<String, Object> payloadJsonMap = objectMapper.readValue(payload, new TypeReference<Map<String, Object>>() {
             });
             var authentication = ViravaAuthenticationToken
                     .ofAuthorized(payloadJsonMap, roleConfig, authTokenConfig, tokenString);
             SecurityContextHolder.getContext().setAuthentication(authentication);
-        } catch (JWTVerificationException | JsonProcessingException exception) {
-            setUnauthorizedAuthContext(tokenString);
-            filterChain.doFilter(request, response);
-            return;
+        } catch (ViravaAuthenticationException | JsonProcessingException exception) {
+            throw new InvalidTokenAuthenticationException(INVALID_ACCESS_TOKEN, exception);
         }
 
         filterChain.doFilter(request, response);
-    }
-
-    private void setUnauthorizedAuthContext(String token) {
-        SecurityContextHolder.getContext().setAuthentication(ViravaAuthenticationToken.ofUnauthorized(token));
-    }
-
-    private DecodedJWT verify(String token, String secret, String issuer) {
-        Algorithm algorithm = Algorithm.HMAC256(secret);
-        JWTVerifier verifier = JWT.require(algorithm)
-                .withIssuer(issuer)
-                .build();
-        return verifier.verify(token);
     }
 }
