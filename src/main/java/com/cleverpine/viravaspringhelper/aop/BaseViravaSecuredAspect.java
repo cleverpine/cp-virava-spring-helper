cdbea1f957844dcc8915c701ad0e4d5fdc74360a
Stoyan Yanev <stoyan.yanev@cleverpine.com>
2024-09-04 11:51:53 +0300

    Remove redundant dependency.

--- src/main/java/com/cleverpine/viravaspringhelper/aop/BaseViravaSecuredAspect.java
+++ src/main/java/com/cleverpine/viravaspringhelper/aop/BaseViravaSecuredAspect.java
@@ -1,11 +1,11 @@
 package com.cleverpine.viravaspringhelper.aop;
 
-import com.cleverpine.cpspringerrorutil.exception.AuthorisationException;
 import com.cleverpine.viravaspringhelper.core.BaseResource;
 import com.cleverpine.viravaspringhelper.core.ViravaAuthenticationToken;
 import com.cleverpine.viravaspringhelper.core.ViravaPrincipalProvider;
 import com.cleverpine.viravaspringhelper.core.ViravaUserPrincipal;
 import com.cleverpine.viravaspringhelper.dto.ScopeType;
+import com.cleverpine.viravaspringhelper.error.exception.ViravaAuthenticationException;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.reflect.MethodSignature;
 
@@ -24,27 +24,27 @@ public class BaseViravaSecuredAspect {
             boolean requireAllResourceIds,
             ScopeType[] scopeList) {
         var authentication = viravaPrincipalProvider.getAuthentication()
-                .orElseThrow(() -> new AuthorisationException("Invalid SecurityContextHolder"));
+                .orElseThrow(() -> new ViravaAuthenticationException("Invalid SecurityContextHolder"));
         var principal = authentication.getPrincipal();
         if (principal == null) {
-            throw new AuthorisationException("Missing ViravaUserPrincipal on method requiring authorisation");
+            throw new ViravaAuthenticationException("Missing ViravaUserPrincipal on method requiring authorisation");
         }
 
         Long resourceId = getMethodSuppliedResourceId(joinPoint, resourceIdParamName);
 
         if (!principal.isAuthorized(resource, resourceId, requireAllResourceIds, scopeList)) {
-            throw new AuthorisationException("User doesn't have required permissions");
+            throw new ViravaAuthenticationException("User doesn't have required permissions");
         }
     }
 
     protected void authorize(JoinPoint joinPoint, BaseResource resource, ScopeType[] scopeList) {
         ViravaAuthenticationToken authentication = this.viravaPrincipalProvider.getAuthentication()
-                .orElseThrow(() -> new AuthorisationException("Invalid SecurityContextHolder"));
+                .orElseThrow(() -> new ViravaAuthenticationException("Invalid SecurityContextHolder"));
         ViravaUserPrincipal principal = authentication.getPrincipal();
         if (principal == null) {
-            throw new AuthorisationException("Missing ViravaUserPrincipal");
+            throw new ViravaAuthenticationException("Missing ViravaUserPrincipal");
         } else if (!principal.isAuthorized(resource, scopeList)) {
-            throw new AuthorisationException("User doesn't have required permissions");
+            throw new ViravaAuthenticationException("User doesn't have required permissions");
         }
     }
 
