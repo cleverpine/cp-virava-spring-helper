011ee4e83f69bfc9506f5ffe86d824bb0f6b2bee
Stoyan Yanev <stoyan.yanev@cleverpine.com>
2024-09-04 11:53:09 +0300

    Add custom response supplier to the authentication entry point.

--- src/main/java/com/cleverpine/viravaspringhelper/error/ViravaAuthErrorEntryPoint.java
+++ src/main/java/com/cleverpine/viravaspringhelper/error/ViravaAuthErrorEntryPoint.java
@@ -1,5 +1,6 @@
 package com.cleverpine.viravaspringhelper.error;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -10,32 +11,55 @@ import org.springframework.security.web.AuthenticationEntryPoint;
 
 import java.io.IOException;
 import java.time.LocalDateTime;
+import java.util.function.Supplier;
 
 public class ViravaAuthErrorEntryPoint implements AuthenticationEntryPoint {
 
-    private final ObjectMapper objectMapper;
+    private ObjectMapper objectMapper;
+    private Supplier<String> responseBodySupplier;
 
+    /**
+     * Creates a new instance of {@link ViravaAuthErrorEntryPoint} with the given {@link ObjectMapper}.
+     * Use this constructor if you want the default error response to be generated.
+     *
+     * @param objectMapper the {@link ObjectMapper} to use for writing the error response
+     */
     public ViravaAuthErrorEntryPoint(ObjectMapper objectMapper) {
         this.objectMapper = objectMapper;
+    }
+
+    /**
+     * Creates a new instance of {@link ViravaAuthErrorEntryPoint} with the given {@link Supplier}.
+     * Use this constructor if you want to provide a custom error response.
+     *
+     * @param responseBodySupplier the supplier to provide the error response
+     */
+    public ViravaAuthErrorEntryPoint(Supplier<String> responseBodySupplier) {
+        this.responseBodySupplier = responseBodySupplier;
     }
 
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException)
             throws IOException {
         var unauthorizedHttpStatus = HttpStatus.UNAUTHORIZED;
-
         response.setStatus(unauthorizedHttpStatus.value());
         response.setContentType(MediaType.APPLICATION_JSON_VALUE);
-
-        var errorResponse = new ViravaErrorResponse(
-                LocalDateTime.now(),
-                HttpServletResponse.SC_UNAUTHORIZED,
-                unauthorizedHttpStatus.getReasonPhrase(),
-                authException.getMessage(),
-                request.getRequestURI());
-
-        response.getWriter()
-                .write(objectMapper.writeValueAsString(errorResponse));
+        response.getWriter().write(getResponseBody(unauthorizedHttpStatus, authException.getMessage(), request.getRequestURI()));
     }
 
+    private String getResponseBody(HttpStatus status, String message, String requestUri) throws JsonProcessingException {
+        String responseBody;
+        if (responseBodySupplier != null) {
+            responseBody = responseBodySupplier.get();
+        } else {
+            var errorResponse = new ViravaErrorResponse(
+                    LocalDateTime.now(),
+                    status.value(),
+                    status.getReasonPhrase(),
+                    message,
+                    requestUri);
+            responseBody = objectMapper.writeValueAsString(errorResponse);
+        }
+        return responseBody;
+    }
 }
